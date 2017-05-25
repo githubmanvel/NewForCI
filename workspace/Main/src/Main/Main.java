b97bd7dab2fd5e3851bece93cff24bb67c625de6
Manvel <Manvel@192.168.1.61>
2017-05-25 08:32:10 +0300

    Main

---
+++ workspace/Main/src/Main/Main.java
@@ -0,0 +1,13 @@
+package Main;
+
+import org.testng.annotations.Test;
+
+public class Main {
+ public static void main(String[] str) {
+	System.out.println("Test");
+}
+ @Test
+ public void firstTest(){
+	 System.out.println("First test");
+ }
+}
